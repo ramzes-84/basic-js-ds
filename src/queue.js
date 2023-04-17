44ff274a42ba81b2bb3a35695ab4ac6e6118b491
Roman Shevyakov <roman@mgstr.ru>
2023-04-17 15:10:16 +0600

    feat: resolve stack + queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
-
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+
+// const test = new ListNode([5,6,7])
+// console.log(test);
+
+
 class Queue {
+  first;
+  last;
 
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.first;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    if (this.first) {
+      this.last.next = new ListNode(value);
+      this.last = this.last.next;
+    } else {
+      this.first = new ListNode(value);
+      this.last = this.first;
+    }
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let neededItem = this.first.value;
+    this.first = this.first.next;
+    return neededItem;
   }
 }
 
