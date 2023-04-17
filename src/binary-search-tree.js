48ec7a8f40748fedbc93c1134e2acdc687f35335
Roman Shevyakov <roman@mgstr.ru>
2023-04-17 22:56:25 +0600

    feat: cant stand it anymore

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -8,12 +8,17 @@ const { NotImplementedError } = require('../extensions/index.js');
 */
 class BinarySearchTree {
 
+  newNode = new Node();
+
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.newNode.data === undefined) {
+      return null
+    } else {
+      return this.newNode;
+    }
   }
 
-  add(/* data */) {
+  add(data) {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
